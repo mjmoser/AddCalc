27130801c58cfbe81a0c75ea4e142d20f1259289
mjmoser <mjanelle720@hotmail.com>
2015-07-09 20:40:46 -0500

    corrections add

--- addCalc.java
+++ addCalc.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
  
-class AddCalc
-{
-   public static void main(String args[])
-   {
+class addCalc {
+   public static void main(String args[]) {
       int x, y, z;
-      System.out.println("Enter two numbers to calculate their sum ");
+	  String answer = "Y";
       Scanner in = new Scanner(System.in);
+	  
+	  while (answer.equals ("Y")) || answer.equals ("y")) {
+	  System.out.println("Enter two numbers to calculate their sum ");
       x = in.nextInt();
       y = in.nextInt();
       z = x + y;
-      System.out.println("Sum of entered numbers = "+z);
-   }
-   
-   
+      System.out.println("Sum of entered numbers = "+z); 
+	  System.out.println("Would you like to add some more? Y or N");
+	  answer = in.next();
+		}
+	}
 }
